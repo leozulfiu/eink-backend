5d86f80e3fbcf4bcdbc2b8246696a29ba8efd787
Leo Zulfiu <16990590+leozulfiu@users.noreply.github.com>
2022-08-18 22:22:10 +0200

    style page with bulma

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,69 @@
 import React from 'react';
 import './App.css';
+import 'bulma/css/bulma.min.css';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-    
-        this.state = {
-            items: [],
-            dataIsLoaded: false
-        };
-    }
+  constructor(props) {
+    super(props);
 
-    componentDidMount() {
-        fetch("api/birthdays")
-            .then((res) => res.json())
-            .then((json) => {
-                this.setState({
-                    items: json,
-                    dataIsLoaded: true
-                });
-            })
-    }
-    render() {
-        const { dataIsLoaded, items } = this.state;
-        if (!dataIsLoaded) return <div>
-            <h1> Pleses wait some time.... </h1> </div> ;
+    this.state = {
+      items: [],
+      dataIsLoaded: false
+    };
+  }
 
-        return (
-            <div className = "App">
-                <h1> Fetch data from an api in react </h1>  {
-                    items.map((item) => (
-                    <ol key = { item.id } >
-                        Full_Name: { item.name },
-                        Birthdate: { item.birthdate }
-                        </ol>
-                    ))
-                }
-            </div>
-        );
-    }
+  componentDidMount() {
+    fetch("api/birthdays")
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({
+          items: json,
+          dataIsLoaded: true
+        });
+      })
+  }
+  render() {
+    const { dataIsLoaded, items } = this.state;
+    if (!dataIsLoaded) return <div>
+      <h1> Pleses wait some time.... </h1> </div>;
+
+    return (
+      <section className="section">
+        <div className="container">
+          <h1 className="title">
+            Birthday management
+          </h1>
+          <p className="subtitle">
+            Manage the birthdates of your friends and family here.
+          </p>
+
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Date of birth</th>
+                <th>Remove</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                items.map((item) => (
+                  <tr key={item.id}>
+                    <td>{item.id}</td>
+                    <td>{item.name}</td>
+                    <td>{item.birthdate}</td>
+                    <td><button className="delete"></button></td>
+                  </tr>
+
+                ))
+              }
+            </tbody>
+          </table>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default App;
