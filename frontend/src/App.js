3ca37c28ef1b8da34d82375a7b5382ab0c0c6451
Leo Zulfiu <16990590+leozulfiu@users.noreply.github.com>
2022-08-21 22:16:28 +0200

    reformat everything

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,8 +17,8 @@ function App() {
     async function fetchData() {
       try {
         await fetch('api/birthdays')
-        .then(response => response.json())
-        .then(data => setBirthdates(data));
+          .then(response => response.json())
+          .then(data => setBirthdates(data));
       } catch (e) {
         console.error('Error fetching api data', e);
       };
@@ -51,26 +51,26 @@ function App() {
           json,
         })));
     })
-    .then(({ status, json, ok }) => {
-      const message = json.message;
-      switch (status) {
-        case 201:
-        case 200:
-          setHasError(false);
-          setSubmitted(true);
-          setSubmittedName(json.name);
-          setBirthdates([...birthdates, {id: json.id, name: json.name, birthdate: json.birthdate}]);
-          setFormValues({
-            name: '',
-            birthdate: ''
-          });
-          break;
-        case 500:
-        default:
-          setHasError(true);
-          setError({status: status, message: JSON.stringify(json)});
-      }
-    });
+      .then(({ status, json, ok }) => {
+        const message = json.message;
+        switch (status) {
+          case 201:
+          case 200:
+            setHasError(false);
+            setSubmitted(true);
+            setSubmittedName(json.name);
+            setBirthdates([...birthdates, { id: json.id, name: json.name, birthdate: json.birthdate }]);
+            setFormValues({
+              name: '',
+              birthdate: ''
+            });
+            break;
+          case 500:
+          default:
+            setHasError(true);
+            setError({ status: status, message: JSON.stringify(json) });
+        }
+      });
   }
 
   const handleNameInputChange = (event) => {
@@ -100,13 +100,13 @@ function App() {
     const d2 = new Date();
 
     let dm = d1.getMonth() - d2.getMonth();
-    let dd = d1.getDate()  - d2.getDate();
-    
+    let dd = d1.getDate() - d2.getDate();
+
     if (dd < 0) { dm -= 1; dd += 30; }
     if (dm < 0) { dm += 12; }
-    
-    const monthsText = `in ${dm} month${dm > 0 ? 's':''} and`;
-    return `${dm > 0 ? monthsText : 'in'} ${dd} day${dd > 1 ? 's':''}`;
+
+    const monthsText = `in ${dm} month${dm > 0 ? 's' : ''} and`;
+    return `${dm > 0 ? monthsText : 'in'} ${dd} day${dd > 1 ? 's' : ''}`;
   }
 
   const getNextBirthday = (date) => {
@@ -116,7 +116,7 @@ function App() {
     birthday.setFullYear(currentDate.getFullYear());
 
     if (birthday - currentDate < 0) {
-        birthday.setFullYear(currentDate.getFullYear() + 1);
+      birthday.setFullYear(currentDate.getFullYear() + 1);
     }
     return birthday;
   }
@@ -132,63 +132,63 @@ function App() {
 
   return (
     <section className="section">
-        <div className="container">
-          <h1 className="title">
-            Birthday management
-          </h1>
-          <p className="subtitle">
-            Manage the birthdates of your friends and family here.
-          </p>
+      <div className="container">
+        <h1 className="title">
+          Birthday management
+        </h1>
+        <p className="subtitle">
+          Manage the birthdates of your friends and family here.
+        </p>
 
-          <div className="columns">
-            <div className="column">
-              <input className="input" type="text" placeholder="Name" value={formValues.name} onChange={handleNameInputChange}/>
-            </div>
-            <div className="column">
-              <input className="input" type="text" placeholder="Birthdate (e.g. 12.7.1999)" value={formValues.birthdate} onChange={handleBirthdateInputChange}/>
-            </div>
-            <div className="column">
+        <div className="columns">
+          <div className="column">
+            <input className="input" type="text" placeholder="Name" value={formValues.name} onChange={handleNameInputChange} />
+          </div>
+          <div className="column">
+            <input className="input" type="text" placeholder="Birthdate (e.g. 12.7.1999)" value={formValues.birthdate} onChange={handleBirthdateInputChange} />
+          </div>
+          <div className="column">
             <button className="button is-primary" onClick={() => createNewBirthday()}>
               Add
             </button>
-            </div>
           </div>
-
-          {submitted && <div className="notification">
-            <button className="delete" onClick={() => closeNotification()}></button>
-            Birthdate with name '{submittedName}' successfully added
-          </div>}
-          {hasError && <div className="notification is-warning">
-            There was an error during submission. {error.status}. {error.message}
-          </div>}
-
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Id</th>
-                <th>Name</th>
-                <th>Date of birth</th>
-                <th>Birthday in</th>
-                <th>Remove</th>
-              </tr>
-            </thead>
-            <tbody>
-              {
-                birthdates.sort(sortDatesAsc).map((birthdate) => (
-                  <tr key={birthdate.id}>
-                    <td>{birthdate.id}</td>
-                    <td>{birthdate.name}</td>
-                    <td>{formatDate(birthdate.birthdate)}</td>
-                    <td>{birthdayIn(birthdate.birthdate)}</td>
-                    <td><button className="delete" onClick={() => deleteRow(birthdate.id)}></button></td>
-                  </tr>
-                ))
-              }
-            </tbody>
-          </table>
         </div>
-      </section>
+
+        {submitted && <div className="notification">
+          <button className="delete" onClick={() => closeNotification()}></button>
+          Birthdate with name '{submittedName}' successfully added
+        </div>}
+        {hasError && <div className="notification is-warning">
+          There was an error during submission. {error.status}. {error.message}
+        </div>}
+
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Date of birth</th>
+              <th>Birthday in</th>
+              <th>Remove</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              birthdates.sort(sortDatesAsc).map((birthdate) => (
+                <tr key={birthdate.id}>
+                  <td>{birthdate.id}</td>
+                  <td>{birthdate.name}</td>
+                  <td>{formatDate(birthdate.birthdate)}</td>
+                  <td>{birthdayIn(birthdate.birthdate)}</td>
+                  <td><button className="delete" onClick={() => deleteRow(birthdate.id)}></button></td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
+    </section>
   );
- }
+}
 
 export default App;
